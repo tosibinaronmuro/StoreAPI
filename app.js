fb911a34469d229efb390bfc1cdf0d6114dc2261
Tosibinaronmuro <tosironj@gmail.com>
2023-02-07 22:26:00 +0100

    set up the basic boiler plate for the project

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
+ require('dotenv').config()
+//  async errors
  const express= require("express")
  const app=express()
+ const notFoundMiddleware=require('./middleware/not-found')
+ const errorMiddleware=require('./middleware/error-handler')
 
+app.use(express.json())
 
+app.get('/',(req,res)=>{
+res.send('<h1>Store Api</h1>')
+})
 
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
 
 port=process.env.PORT || 5000
-app.createServer(port,()=>{
-    
-})
+const start= async ()=>{
+    try {
+        // connect DB
+        app.listen(port,()=>{
+            console.log(`listening on port ${port}`)
+        })
+        
+    } catch (error) {
+        
+    }
+}
+start()
